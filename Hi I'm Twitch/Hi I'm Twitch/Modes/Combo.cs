c347d3ad396fb3fbcfc6ba26aedf17f457de4e1e
GinjiBan <muginji@hotmail.fr>
2016-03-13 21:15:29 +0100

    Item usage

--- Hi I'm Twitch/Hi I'm Twitch/Modes/Combo.cs
+++ Hi I'm Twitch/Hi I'm Twitch/Modes/Combo.cs
@@ -12,15 +12,35 @@ namespace AddonTemplate.Modes
             return Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo);
         }
 
+        private void ItemUsage()
+        {
+            var target = TargetSelector.GetTarget(550, DamageType.Physical); // 550 = Botrk.Range
+            if (Settings.UseYoumuu && Config.Youmuu.IsOwned() && Config.Youmuu.IsReady())
+            {
+                Config.Youmuu.Cast();
+            }
+            if (target != null)
+            {
+                if (Settings.useBotrk && Item.HasItem(Config.Cutlass.Id) && Item.CanUseItem(Config.Cutlass.Id) &&
+                    Player.Instance.HealthPercent < Settings.MinHPBotrk &&
+                    target.HealthPercent < Settings.EnemyMinHPBotrk)
+                {
+                    Item.UseItem(Config.Cutlass.Id, target);
+                }
+                if (Settings.useBotrk && Item.HasItem(Config.Botrk.Id) && Item.CanUseItem(Config.Botrk.Id) &&
+                    Player.Instance.HealthPercent < Settings.MinHPBotrk &&
+                    target.HealthPercent < Settings.EnemyMinHPBotrk)
+                {
+                    Config.Botrk.Cast(target);
+                }
+            }
+        }
+
         public override void Execute()
         {
             if (Settings.UseQ && Q.IsReady())
             {
-                var target = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
-                if (target != null)
-                {
-                    
-                }
+                ItemUsage();
             }
         }
     }
